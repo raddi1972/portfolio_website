1d9d2f6fca0b9fa5d8e67639eeab81980622b4b2
Rudransh <dixitrudransh01@gmail.com>
2022-09-05 12:17:37 +0530

    Completed the template

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Banner } from './components/Banner';
-import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
-import Navbar from './components/Nvabar';
+import {NavBar} from './components/Nvabar';
 import { Projects } from './components/Projects';
 import { Skills } from './components/Skills';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <NavBar/>
       <Banner/>
       <Skills/>
       <Projects />
-      <Contact />
       <Footer />
     </div>
   );
