e05225a81add3beddf8780717f36cee9ecf6d9e1
Rudransh <dixitrudransh01@gmail.com>
2022-09-05 11:58:46 +0530

    Added Contact.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Banner } from './components/Banner';
 import Navbar from './components/Nvabar';
+import { Projects } from './components/Projects';
 import { Skills } from './components/Skills';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Banner/>
       <Skills/>
+      <Projects />
     </div>
   );
 }
