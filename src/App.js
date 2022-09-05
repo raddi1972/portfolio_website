e522fa5d0b7ce3ba9100fd28dca68c10ab32c9ef
Rudransh <dixitrudransh01@gmail.com>
2022-09-05 11:57:18 +0530

    Added components to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Banner } from './components/Banner';
 import Navbar from './components/Nvabar';
+import { Skills } from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Banner/>
+      <Skills/>
     </div>
   );
 }
